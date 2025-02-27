30269e5a89c4142b34062230a6e75dd4135153c5
Maarten Balliauw <maarten@maartenballiauw.be>
2025-02-27 16:33:55 -0500

    Add some options and mermaid support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,31 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator';
 import starlightVersions from 'starlight-versions';
+import starlightClientMermaid from '@pasqal-io/starlight-client-mermaid'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			plugins: [
+				starlightClientMermaid({ /* options */ }),
 				starlightLinksValidator(),
 				/*starlightVersions({
 					versions: [{ slug: '1.0' }],
 				}),*/
 			],
-			title: 'My Docs',
+			title: 'Duende Software Docs',
+			logo: {
+				src: './src/assets/duende-logo.png',
+				replacesTitle: true,
+			},
+			editLink: {
+				baseUrl: 'https://github.com/DuendeSoftware/docs.duendesoftware.com/edit/main/docs/',
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/DuendeSoftware',
+				linkedin: 'https://www.linkedin.com/company/duendesoftware/',
+				'x.com': 'https://x.com/DuendeIdentity',
 			},
 			sidebar: [
 				{
@@ -29,6 +40,18 @@ export default defineConfig({
 				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
+				},
+				{
+					label: 'IdentityServer',
+					autogenerate: { directory: 'identityserver' },
+				},
+				{
+					label: 'Backend-for-Frontend',
+					autogenerate: { directory: 'bff' },
+				},
+				{
+					label: 'Open-source',
+					autogenerate: { directory: 'foss' },
 				},
 			],
 		}),
